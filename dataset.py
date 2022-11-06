a6bdf34426633e77e041c452fa8cdea84b3cc4e1
mdtayebadnan <111030551+mdtayebadnan@users.noreply.github.com>
2022-11-06 20:20:06 +0900

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,29 @@
+class CarvanaDataset(Dataset):
+    def __init__(self,img_list,mask_list,transform=None):
+        self.img_list = img_list
+        self.mask_list = mask_list
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.img_list)
+    
+    def __getitem__(self,index):
+        img_path = os.path.join(ROOTDIR.train,self.img_list[index])
+        mask_path = os.path.join(ROOTDIR.train_mask,self.mask_list[index])
+        img = Image.open(img_path)
+        mask = Image.open(mask_path)
+        img = np.array(img)
+        mask = np.array(mask)
+        mask[mask==255.0] = 1.0
+        #img_mask_dict = {"image": img, "mask": mask}
+        
+        if self.transform:
+            augmentation = self.transform(image=img, mask=mask)
+            img = augmentation["image"]
+            mask = augmentation["mask"]
+            mask = torch.unsqueeze(mask,0)
+            #transformations = self.transform(image=img, mask=mask)
+            #img = transformations["image"]
+            #mask = transformations["mask"]
+            
+        return img,mask
